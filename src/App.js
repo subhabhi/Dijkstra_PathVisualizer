4020568e4cafec7723d2730a16bc0a36b7ef78bf
Abhishek Shubham <108691368+subhabhi@users.noreply.github.com>
2023-08-16 23:09:29 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import PathfindingVisualizer from "./PathfindingVisualizer/PathfindingVisualizer";
+import {Tutorial} from "./PathfindingVisualizer/Tutorial/Tutorial";
+
+
+function App() {
+  return (
+    <div className="App">
+      <PathfindingVisualizer></PathfindingVisualizer>
+      <div className="PopUp">
+        <Tutorial />
+      </div>
+    </div>
+    
+  );
+}
+
+export default App;
